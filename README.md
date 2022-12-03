99465335fea95ed3ee90201f45948f72be19fa30
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-12-04 06:28:09 +0800

    personalize frontend
    - add textarea to enter text
    - add button to proceed
    - change title and subtitle
    - get textbox onChange event

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # buildspace 
 ### Welcome 👋
 This is the starter template for the [build your own AI writing assistant w/ GPT-3](https://buildspace.so/builds/ai-writer) project.
+
+### Project Setup
+
+`cd` into the project, install `next`, `react`, and `react-dom` and then run yarn to get the project going.
+
+#### cd into the repo
+```
+cd gpt3-writer-starter
+```
+
+#### install next if you don't have it
+```
+yarn add next react react-dom
+```
+
+#### run it
+```
+yarn dev
+```
+
+Then, head to [localhost:3000](http://localhost:3000) and you should see the following:
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  
   return (
     <div className="root">
       <Head>
@@ -11,10 +19,25 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Generate Quality Tweets</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Insert keypoints to automatically turn it into a tweet!</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="start typing here" 
+            className="prompt-box" 
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
